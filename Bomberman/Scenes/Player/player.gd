bdfe9feabb4eaf52b74237915126ee1cd9ee14d9
Gustavo Ordoñez <gustavoalberto117@gmail.com>
2025-12-07 23:26:19 -0300

    Add logic to collision

--- Bomberman/Scenes/Player/player.gd
+++ Bomberman/Scenes/Player/player.gd
@@ -5,10 +5,16 @@ class_name Player
 @export var moveSpeed: float = 75
 
 var direction: Vector2 = Vector2.ZERO
+
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var raycasts: Raycasts = $Raycasts
+
+var movement: Vector2 = Vector2.ZERO
 
 func _process(delta: float) -> void:
-
+	var collisions = raycasts.check_collisions()
+	if collisions.has(movement):
+		return
 	position += direction * delta * moveSpeed
 
 func _input(event: InputEvent) -> void:
